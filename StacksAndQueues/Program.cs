b290c68962180c5f4c8ff38481e405732f4550dc
eswar <neelaveni.eswar04@gmail.com>
2022-04-23 09:52:57 +0530

    [Eswar] Welcome to Stacks And Queues

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks And Queues");
+            Console.ReadLine();
+        }
+    }
+}
