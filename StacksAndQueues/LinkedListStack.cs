97979b11e6abb9e4bdab1592e26cc3e28269fbde
eswar <neelaveni.eswar04@gmail.com>
2022-04-23 10:06:28 +0530

    [Eswar] Added LinkedList stack

---
+++ StacksAndQueues/LinkedListStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        internal void Push(int value)
+        {
+            ///Creating a link between new node and where top is pointing
+            ///Then pointing Top towords the new node
+            ///new node is inserted on the top
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+
+        }
+    }
+}
+    
+
--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -12,6 +12,11 @@ namespace StacksAndQueues
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks And Queues");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
             Console.ReadLine();
         }
     }
